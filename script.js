9717c50593390ff976d83feb16b247d1f6c7d98d
ashimaganja <ashimaganja98@gmail.com>
2025-01-26 12:30:25 -0500

    Some JS and DOM

---
+++ script.js
@@ -0,0 +1,16 @@
+const playground = document.getElementById('playground');
+
+//querySelector gets the first tag that matches the query
+
+
+const magicalOrb = document.createElement('div');
+magicalOrb.textContent = '🔮';
+
+playground.append(magicalOrb);
+
+
+
+
+
+
+
